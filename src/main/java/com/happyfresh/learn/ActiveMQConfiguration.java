d681fd2b4e3663d382b8279ae8c7475557aeb23d
Aldo Susanto <aldosusanto@hotmail.com>
2021-04-03 09:35:55 +0700

    initial commit, kafka and activeMQ setted up

---
+++ src/main/java/com/happyfresh/learn/ActiveMQConfiguration.java
@@ -0,0 +1,33 @@
+package com.happyfresh.learn;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.core.JmsTemplate;
+
+import javax.jms.Queue;
+
+@Configuration
+public class ActiveMQConfiguration {
+
+    private String brokerUrl = "tcp://localhost:61616";
+
+    @Bean
+    public Queue queue(){
+        return new ActiveMQQueue("test-queue");
+    }
+
+    @Bean
+    public ActiveMQConnectionFactory ActiveMQCustomConnectionFactory(){
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory();
+        factory.setBrokerURL(brokerUrl);
+        return factory;
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate(){
+        return new JmsTemplate(ActiveMQCustomConnectionFactory());
+    }
+}
---
+++ src/main/java/com/happyfresh/learn/TestController.java
@@ -0,0 +1,43 @@
+package com.happyfresh.learn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.jms.Queue;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    @Autowired
+    private Queue queue;
+
+    public static final String TOPIC = "test.topic";
+
+    @GetMapping("/")
+    public String testAPI(){
+        return "hello world !";
+    }
+
+    @GetMapping("/kafka/{message}")
+    public String testAPIKafka(@PathVariable String message){
+//        kafkaTemplate.send(TOPIC, message); // Default configs on the kafka (localhost:9092)
+        return message;
+    }
+
+    @GetMapping("/mq/{message}")
+    public String testAPIQueue(@PathVariable String message){
+        jmsTemplate.convertAndSend(queue, message);
+        return message;
+    }
+
+}
